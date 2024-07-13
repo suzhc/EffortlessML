de8f0e6fdbe82ce4d3410dfdeb4f1f8ea2d56988
suzhao <suzhao10086@gmail.com>
2024-07-13 10:27:01 +0800

    dataset module

---
+++ EffortlessML/dataset.py
@@ -0,0 +1,13 @@
+from sklearn import datasets
+
+from EffortlessML.data import MLData
+
+
+def get_breast_cancer():
+    '''
+    https://archive.ics.uci.edu/dataset/17/breast+cancer+wisconsin+diagnostic
+    '''
+    dataset = datasets.load_breast_cancer(as_frame=True)
+    x_cols = dataset['feature_names']
+    y_col = 'target'
+    return MLData(df=dataset['frame'], x_cols=x_cols, y_col=y_col)
